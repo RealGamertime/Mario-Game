f4784b776eb522830463bd2a784db702fbc68a8e
Nicholas Carr <blueant15@gmail.com>
2017-12-12 20:46:02 -0800

    Added a lot of stuff.

--- Mario Redux/src/Graphics.java
+++ Mario Redux/src/Graphics.java
@@ -1,3 +1,4 @@
+import static org.lwjgl.opengl.GL11.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,6 +13,8 @@ public class Graphics
 		 * TODO
 		 * Make it so key has the extension name in it and this will find the extension value.
 		 */
+		if(key.equals(""))
+			return null;
 		try {
 			return TextureLoader.getTexture("png", new FileInputStream(new File("res/" + key + ".png")));
 		}
@@ -20,5 +23,41 @@ public class Graphics
 			e.printStackTrace();
 		}
 		return null;
+	}
+	public static void drawRect(float x, float y, float width, float height)
+	{
+		drawRect(x,y,width,height, 0, "");
+	}
+	public static void drawRect(float x, float y, float width, float height, String tex)
+	{
+		drawRect(x,y,width,height, 0, tex);
+	}
+	public static void drawRect(float x, float y, float width, float height, float rot)
+	{
+		drawRect(x,y,width,height, rot, "");
+	}
+	public static void drawRect(float x, float y, float width, float height, float rot, String tex)
+	{
+		
+		Texture tempTex = loadTexture(tex);
+		if(tempTex != null)
+		{
+			tempTex.bind();
+		}
+		glPushMatrix();
+		{
+			glTranslatef(x,y,0);
+			glRotatef(rot,0,0,1);
+			
+			glBegin(GL_QUADS);
+			{
+				glVertex2f(0, 0);
+				glVertex2f(0, height);
+				glVertex2f(width, height);
+				glVertex2f(width, 0);
+			}
+			glEnd();
+		}
+		glPopMatrix();
 	}
 }
--- Mario Redux/src/Main.java
+++ Mario Redux/src/Main.java
@@ -25,21 +25,18 @@ public class Main {
 		gameLoop();
 		cleanUp();
 		
-		
 	}
 	
 	private static void gameLoop()
 	{
-		/*Texture text = Graphics.loadTexture("Mario_brick");
-		text.bind();*/
 		
 		//Game Loop
 		while(!Display.isCloseRequested())
 		{
 			glClear(GL_COLOR_BUFFER_BIT);
 			glLoadIdentity();
-			drawRect(56, 56, 200, 20);
-			drawRect(56, 56, 200, 50, 90);
+			Graphics.drawRect(56, 56, 336, 336, "Mario_Brick");
+			//Graphics.drawRect(56, 56, 200, 50, 90);
 			Display.update();
 		}
 	}
@@ -76,27 +73,5 @@ public class Main {
 		{
 			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
 		}
-	}
-	private static void drawRect(float x, float y, float width, float height)
-	{
-		drawRect(x,y,width,height, 0);
-	}
-	private static void drawRect(float x, float y, float width, float height, float rot)
-	{
-		glPushMatrix();
-		{
-			glTranslatef(x,y,0);
-			glRotatef(rot,0,0,1);
-			
-			glBegin(GL_QUADS);
-			{
-				glVertex2f(0, 0);
-				glVertex2f(0, height);
-				glVertex2f(width, height);
-				glVertex2f(width, 0);
-			}
-			glEnd();
-		}
-		glPopMatrix();
 	}
 }
